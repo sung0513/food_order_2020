93fab6e4d269a4984ff051597fb6bd03201a24bf
sung0513 <rlatjddls367@naver.com>
2020-02-01 11:47:33 +0900

    https://github.com/sung0513/food_order_2020.git

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food( name, pictrue) {
+  return (
+    <div> 
+      <h2>주문 리스트 확인</h2>
+        <img src={pictrue} alt={name} />
+    </div>
+  )
+}
+
+
+//component의 재사용성
+const foodIlike = [
+  {
+    id:1,
+    name:"치킨",
+    image:
+      "http://www.momstouch.co.kr/data/shopimages/xboard/menu/20170825527729.jpg"
+  },
+  
+  {
+    id:2,
+    name:"피자",
+    image: "https://cdn.dominos.co.kr/admin/upload/goods/20200119_h150V2hn.jpg" 
+  },
+  
+  {
+    id:3,
+    name: "1인 배달",
+    image: "https://steemitimages.com/DQmZQcAQYCxHKLtomTLdjkMBqiAc5yZqq4dKwK6wZNvSeq4/44.jpg"
+  },
+  {
+    id:4,
+    name:"뭐뭐",
+    image: "http://www.momstouch.co.kr/data/shopimages/xboard/menu/20170825900916.jpg"
+  }
+];  
+//map이 랜더링 조집니다. 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+         {foodIlike.map(dish => (
+           <Food key={dish.id} name={dish.name} pictrue={dish.image} />  
+         ))}
     </div>
   );
 }
 
+
+
 export default App;
+
+//props => chicken이라고 전달함
+
+//리액트란 사용하는 모든 요소를 생성한다.
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+//component html를 반환하는 함수이다. 매우 eazy함 jsx -> react의 유일한 개념 
+//react application이 하나의 component만을 랜더링한다. 
